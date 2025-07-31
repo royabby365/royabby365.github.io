cb9c1d1bdf75b54c415b7cdf82f0fb61b580f8fd
Roy Abernathy <roy.u.abernathy@gmail.com>
2025-07-30 22:57:07 -0500

    Update submit.php
    Updated email stuff

--- submit.php
+++ submit.php
@@ -14,10 +14,10 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'roy.u.abernathy@gmail.com';
+$to = 'roy@royabernathy.info';
 $email_subject = "Email Received From Ethanol Theme:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@royabby.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@royabernathy.info\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
